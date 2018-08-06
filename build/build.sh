cfaf4e99afd25bf52544cbc112be57184eca36af
Peter Sagerson <psagers@ignorare.net>
2018-08-05 20:48:12 -0700

    Cleanup and cabal constraint fix.

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 SCRIPT=`readlink -f $0`
+PATH=/usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin:/usr/local/ghc7/bin:$PATH
 
 install_ghc7 ()
 {
@@ -27,7 +28,7 @@ install_cabal ()
 
     cd /tmp/cabal/cabal-install
     env CC=gcc GHC=/usr/local/ghc7/bin/ghc GHC_PKG=/usr/local/ghc7/bin/ghc-pkg PREFIX=/usr/local/ghc7 \
-        ./bootstrap.sh --global
+        ./bootstrap.sh --global --no-doc
 }
 
 build_elm ()
@@ -37,24 +38,19 @@ build_elm ()
 
     fetch https://raw.githubusercontent.com/elm-lang/elm-platform/master/installers/BuildFromSource.hs
     if patch < `dirname $SCRIPT`/patch-BuildFromSource.hs; then
-        /usr/bin/env PATH=/usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin:$PATH /usr/local/ghc7/bin/runhaskell BuildFromSource.hs 0.18
+        runhaskell BuildFromSource.hs 0.18
     fi
 }
 
 link_elm ()
 {
-    [ -e /usr/local/bin/elm ] || ln -s /usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin/elm /usr/local/bin/elm
-    [ -e /usr/local/bin/elm-make ] || ln -s /usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin/elm-make /usr/local/bin/elm-make
-    [ -e /usr/local/bin/elm-package ] || ln -s /usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin/elm-package /usr/local/bin/elm-package
-    [ -e /usr/local/bin/elm-reactor ] || ln -s /usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin/elm-reactor /usr/local/bin/elm-reactor
-    [ -e /usr/local/bin/elm-repl ] || ln -s /usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin/elm-repl /usr/local/bin/elm-repl
+    for cmd in /usr/local/elm/Elm-Platform/0.18/.cabal-sandbox/bin/elm*; do
+        [ -e /usr/local/bin/`basename $cmd` ] || ln -s $cmd /usr/local/bin/
+    done
 }
-
 
 pkg install -y ca_root_nss compat8x-amd64 gcc gmake perl5 libiconv git
 
 [ -e /usr/local/ghc7/bin/ghc ] || install_ghc7
-PATH=/usr/local/ghc7/bin:$PATH
 [ -e /usr/local/ghc7/bin/cabal ] || install_cabal
-build_elm
-link_elm
+build_elm && link_elm
--- build/patch-BuildFromSource.hs
+++ build/patch-BuildFromSource.hs
@@ -1,10 +1,12 @@
---- BuildFromSource.hs.orig	2018-08-05 16:13:09.710412000 -0700
-+++ BuildFromSource.hs	2018-08-05 16:15:22.215205000 -0700
-@@ -198,6 +198,7 @@
-       -- install all of the packages together in order to resolve transitive dependencies robustly
-       -- (install the dependencies a bit more quietly than the elm packages)
-       cabal ([ "install", "-j", "--only-dependencies", "--ghc-options=\"-w\"" ]
-+             ++ ["--constraint=basement<0.0.8"]
-              ++ (if version <= "0.15.1" then [ "--constraint=fsnotify<0.2" ] else [])
-              ++ map fst repos)
-       cabal ([ "install", "-j" ]
+--- BuildFromSource.hs.orig	2018-08-05 16:37:24.434519000 -0700
++++ BuildFromSource.hs	2018-08-05 16:51:34.234050000 -0700
+@@ -183,7 +183,8 @@
+ makeRepos artifactDirectory version repos =
+   do  createDirectoryIfMissing True artifactDirectory
+       setCurrentDirectory artifactDirectory
+-      writeFile "cabal.config" "split-objs: True"
++      writeFile "cabal.config" "split-objs: True\n"
++      appendFile "cabal.config" "constraints: basement<0.0.8\n"
+       root <- getCurrentDirectory
+       mapM_ (uncurry (makeRepo root)) repos
+ 
